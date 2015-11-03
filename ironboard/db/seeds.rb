f062acf51a57ce9ec78ffe3b071610f7045ed96d
github username <github email address>
2015-11-03 17:54:31 -0500

    modified seed.db to work after creating host and guest models

--- ironboard/db/seeds.rb
+++ ironboard/db/seeds.rb
@@ -6,9 +6,18 @@ Tag.delete_all
 Rsvp.delete_all
 EventTag.delete_all
 
-user1 = User.create(name: "Reuben", location: "11218", bio: "good stuff", img_url: "board#{rand(1..10)}.jpeg")
-user2 = User.create(name: "Roslynn", location: "11218", bio: "more good stuff", img_url: "board#{rand(1..10)}.jpeg")
+user1 = User.create(name: "Reuben", location: "11218", bio: "good stuff", 
+	img_url: "board#{rand(1..10)}.jpeg", password: "password", password_confirmation: "password")
+user2 = User.create(name: "Roslynn", location: "11218", bio: "more good stuff", 
+	img_url: "board#{rand(1..10)}.jpeg", password: "password", password_confirmation: "password")
 
+user1.build_host
+user1.build_guest
+user1.save
+
+user2.build_host
+user2.build_guest
+user2.save
 
 event1 = user1.host.events.create(title: "Pizza Party", location: "Flatiron", day: "2015-11-07", start_time: "14:00", end_time: "16:00")
 event2 = user2.host.events.create(title: "Ping Pong Whiskey", location: "Flatiron", day: "2015-11-09", start_time: "18:00", end_time: "21:00")
@@ -17,12 +26,8 @@ event2 = user2.host.events.create(title: "Ping Pong Whiskey", location: "Flatiro
 Tag.create(name: "Food")
 Tag.create(name: "Funtimes")
 
-
 user1.guest.events << event2
 user2.guest.events << event1
-# Rsvp.create(guest_id: 1, event_id: 1)
-# Rsvp.create(guest_id: 2, event_id: 1)
-# Rsvp.create(guest_id: 2, event_id: 2)
 
 EventTag.create(event_id: 1, tag_id: 1)
 EventTag.create(event_id: 2, tag_id: 2)
