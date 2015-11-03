fed6526c286dabd43a5764e964b326d0805f73b3
tatsianab <tnborshch@yahoo.com>
2015-11-03 11:59:45 -0500

    Logout,login,helper,signup

--- ironboard/db/schema.rb
+++ ironboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151102203900) do
+ActiveRecord::Schema.define(version: 20151103145715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20151102203900) do
     t.string "bio"
     t.string "location"
     t.string "img_url"
+    t.string "password_digest"
+    t.string "email"
   end
 
 end
