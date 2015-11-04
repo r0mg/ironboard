ddd8693caf4ed222a3d3dc46de09c758056faa26
matthewbillienyc <matthewbillienyc@gmail.com>
2015-11-04 09:56:42 -0500

    stash

--- ironboard/db/seeds.rb
+++ ironboard/db/seeds.rb
@@ -10,6 +10,8 @@ user1 = User.create(name: "Reuben", location: "11218", bio: "good stuff",
 	img_url: "board#{rand(1..10)}.jpeg", password: "password", password_confirmation: "password")
 user2 = User.create(name: "Roslynn", location: "11218", bio: "more good stuff", 
 	img_url: "board#{rand(1..10)}.jpeg", password: "password", password_confirmation: "password")
+user3 = User.create(name: "matthew", location: "11211", bio: "yeah", password: "a", password_confirmation: "a")
+
 
 user1.build_host
 user1.build_guest
@@ -18,6 +20,10 @@ user1.save
 user2.build_host
 user2.build_guest
 user2.save
+
+user3.build_host
+user3.build_guest
+user3.save
 
 event1 = user1.host.events.create(title: "Pizza Party", location: "Flatiron", day: "2015-11-07", start_time: "14:00", end_time: "16:00")
 event2 = user2.host.events.create(title: "Ping Pong Whiskey", location: "Flatiron", day: "2015-11-09", start_time: "18:00", end_time: "21:00")
