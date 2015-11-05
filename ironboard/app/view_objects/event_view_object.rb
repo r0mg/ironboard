cad043661353b01c76dd194e4883536d35e674c2
github username <github email address>
2015-11-05 17:41:44 -0500

    improved events show page using bootstrap

--- ironboard/app/view_objects/event_view_object.rb
+++ ironboard/app/view_objects/event_view_object.rb
@@ -8,6 +8,7 @@ class EventViewObject
   	am_pm = "AM"
 		am_pm = "PM" if @event.start_time.hour>=12
 		hour = "#{@event.start_time.hour%12}"
+    hour = '0'+ hour  if hour.length == 1
   	minutes = "#{@event.start_time.min}"
   	minutes = '0'+minutes if minutes.length == 1
   	"#{hour}:#{minutes} #{am_pm}"
@@ -17,9 +18,22 @@ class EventViewObject
 		am_pm = "AM" 	
 		am_pm = "PM" if @event.end_time.hour>12
 		hour = "#{@event.end_time.hour%12}"
+    hour = '0'+ hour  if hour.length == 1
   	minutes = "#{@event.end_time.min}"
   	minutes = '0'+minutes if minutes.length == 1
   	"#{hour}:#{minutes} #{am_pm}"
+  end
+
+  def guests_attending
+    num_guests = @event.guests.count
+    case num_guests
+    when 0
+      "No guests have RSVP'd yet!"
+    when 1
+      "1 guest has RSVP'd: "
+    else
+      "#{num_guests} guests have RSVP'd: "
+    end
   end
 
 end
