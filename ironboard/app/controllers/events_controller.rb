dff7e3dd51a3ec4c3de667af7359dc6bdb9ffb0c
matthewbillienyc <matthewbillienyc@gmail.com>
2015-11-06 10:44:44 -0500

    fixed update event bug

--- ironboard/app/controllers/events_controller.rb
+++ ironboard/app/controllers/events_controller.rb
@@ -38,6 +38,7 @@ class EventsController < ApplicationController
   def update
     event = Event.find(params[:id])
     event.update(event_params)
+    event.save
     redirect_to event_path(event)
   end
 
